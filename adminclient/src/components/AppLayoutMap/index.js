a79cc8b20349f8581c2970ddf1f7de940cd53028
Yaw Joseph Etse <yaw.etse@gmail.com>
2016-12-22 18:01:41 -0500

    saved layout generator

---
+++ adminclient/src/components/AppLayoutMap/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as rebulma from 're-bulma'; // FormHorizontal, NavToggle, ControlLabel, Group,
+// import * as reactdom from 'react-dom';
+//https://github.com/lolJS/react-animate.css/blob/master/src/app.js
+
+let AppLayoutMap = Object.assign({},React.DOM,rebulma);
+
+export default AppLayoutMap;
--- adminclient/src/containers/login.js
+++ adminclient/src/containers/login.js
@@ -1,23 +1,65 @@
-import React, { Component } from 'react';
-import { Hero, HeroBody, Title, Subtitle, Container, } from 're-bulma';
-import 'font-awesome/css/font-awesome.css';
+import React, { Component, createElement, } from 'react';
+// import { Hero, HeroBody, Title, Subtitle, Container, } from 're-bulma';
+// import 'font-awesome/css/font-awesome.css';
+import AppLayoutMap from '../components/AppLayoutMap';
 
 let loginLayout = {
   component: 'Hero',
-  props: { size: 'isFullheight', },
+  props: {
+    size: 'isFullheight',
+    // children: 'hero body text',
+  },
   children: [ {
     component: 'HeroBody',
+    props: {
+      // children: 'hero body text'
+    },
     children: [ {
       component: 'Container',
+      props: {
+        // children: 'container text'
+      },
       children: [ {
         component: 'Title',
-        innerText: 'Login',
-      },{
-        component: 'Hero subtitle prop',
-        innerText: 'Login',
+        // props: {
+        // },
+        children: 'Login',
+      }, {
+        component: 'Subtitle',
+        children: 'Subtitle Login',
+        //   props: {
+        // },  
       }]
     }]
   }]
+};
+
+let sampleText = {
+  component: 'Hero',
+  props: { size: 'isFullheight', children:'text from children' },
+  innerText: 'this should be in a div'
+}
+let renderIndex = 0;
+
+function getRenderedComponent(componentObject) {
+  renderIndex++;
+  console.log({ componentObject, renderIndex });
+  // if (componentObject.children) {
+  //   console.log(componentObject.component + ' has children');
+  //   return componentObject.children.map(childComponentObject => getRenderedComponent(childComponentObject));
+  // }
+  // else {
+    let renderedCompProps = Object.assign({key:renderIndex}, componentObject.props);
+    console.log(componentObject.component + ' is rendering, with props',renderedCompProps);
+    // return createElement(AppLayoutMap[ componentObject.component ],renderedCompProps);
+    return createElement(
+      AppLayoutMap[ componentObject.component ],
+      renderedCompProps,
+      (Array.isArray(componentObject.children) && typeof componentObject.children !=='string') ?
+        componentObject.children.map(childComponentObject => getRenderedComponent(childComponentObject)) :
+        componentObject.children
+    );
+  // }
 }
 
 // class ResponsiveGrid extends Component {
@@ -37,17 +79,7 @@ class Login extends Component {
     // this.setState(nextProps);
   }
   render() {
-    return (
-      <Hero size="isFullheight">
-        <HeroBody>
-          <Container>
-            <Title>Login</Title>
-            <Subtitle>Hero subtitle</Subtitle>
-            {this.props.children}
-          </Container>
-        </HeroBody>
-      </Hero>
-    );
+    return getRenderedComponent(loginLayout);
   }
 }
 
