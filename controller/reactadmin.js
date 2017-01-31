94b4f11a0632902b1ae46bc4ec237694648ebd16
janbialostok <janbialostok@gmail.com>
2017-01-31 18:45:06 -0500

    init

--- controller/reactadmin.js
+++ controller/reactadmin.js
@@ -1,6 +1,6 @@
 'use strict';
 const Promisie = require('promisie');
-const fs = require('fs-extra');
+const fs = Promisie.promisifyAll(require('fs-extra'));
 const path = require('path');
 const MANIFEST = require(path.join(__dirname, '../adminclient/src/content/config/manifest'));
 const NAVIGATION = require(path.join(__dirname, '../adminclient/src/content/config/navigation'));
@@ -26,6 +26,9 @@ var logger;
 var appSettings;
 var appenvironment;
 var CoreUtilities;
+var manifestSettings;
+var navigationSettings;
+var periodic;
 
 var determineAccess = function (privileges, layout) {
   if (!privileges.length && (!layout.privileges || !layout.privileges.length)) return true;
@@ -43,10 +46,50 @@ var determineAccess = function (privileges, layout) {
 };
 
 var removeNullIndexes = function (data) {
-  for (let i = 0; i < data.length; i++) {
-    if (!data[i]) data.splice(i, 1);
+  let index = data.indexOf(null);
+  while (index > -1) {
+    data.splice(index, 1);
+    index = data.indexOf(null);
   }
   return data;
+};
+
+var readConfigurations = function (filePath) {
+  filePath = path.join(process.cwd(), filePath);
+  let _import = function (_path) {
+    if (path.extname(_path) === '.js') return Promisie.resolve(require(_path));
+    else return fs.readJsonAsync(_path);
+  };
+  return fs.statAsync(filePath)
+    .then(stats => {
+      if (stats.isFile()) return _import(filePath);
+      else if (stat.isDirectory()) {
+        return fs.readdirAsync(filePath)
+          .then(files => {
+            return Promisie.map(files, file => {
+              let fullPath = path.join(filePath, file);
+              return _import(fullPath);
+            });
+          })
+          .catch(e => Promisie.reject(e));
+      }
+      else return Promisie.reject(new TypeError('Configuration path is not a file or directory'));
+    })
+    .catch(e => Promisie.reject(e));
+};
+
+var readAndStoreConfigurations = function (paths) {
+  let reads = paths.map(_path => readConfigurations.bind(null, _path));
+  return Promisie.settle(reads)
+    .then(result => {
+      let { fulfilled, rejected } = result;
+      return fulfilled.reduce((result, value) => {
+        if (Array.isArray(value)) return result.concat(value);
+        result.push(value);
+        return result;
+      }, []);
+    })
+    .catch(e => Promisie.reject(e));
 };
 
 var recursivePrivilegesFilter = function (privileges, config = {}, isRoot = false) {
@@ -119,19 +162,43 @@ var loadUserPreferences = function (req, res) {
   });
 };
 
-var loadNavigation = function (req, res) {
-  let navigation = NAVIGATION;
-  navigation.layout = recursivePrivilegesFilter(Object.keys(req.session.userprivilegesdata), [navigation.layout])[0];
-  res.status(200).send({
-    result: 'success',
-    status: 200,
-    data: {
-      settings: NAVIGATION,
-    },
-  });
+var pullNavigationData = function () {
+  if (navigationSettings) return Promisie.resolve(navigationSettings);
+  let _navigationSettings = periodic.app.locals.extension.reactadmin.settings.navigation;
+  let _themeSettings = (periodic.app.themeconfig && periodic.app.themeconfig.settings) ? periodic.app.themeconfig.settings : false;
+  if (_themeSettings) _navigationSettings = (themeSettings.navigation) ? themeSettings.navigation : navigationSettings;
+  return readAndStoreConfigurations((Array.isArray(_navigationSettings)) ? _navigationSettings : [_navigationSettings])
+    .then(result => {
+      let navigation = result.reduce((final, config) => {
+        final.wrapper = Object.assign(final.wrapper || {}, config.wrapper);
+        final.container = Object.assign(final.container || {}, config.container);
+        final.layout = final.layout || {};
+        final.layout.children = (Array.isArray(final.layout.children)) ? final.layout.children.concat(config.layout.children || []) : config.layout.children;
+        return final;
+      }, {});
+      navigationSettings = navigation;
+      return navigationSettings;
+    })
+    .catch(e => Promisie.reject(e));
+};
+
+var loadNavigation = function (req, res, next) {
+  pullNavigationData()
+    .then(navigation => {
+      navigation.layout = recursivePrivilegesFilter(Object.keys(req.session.userprivilegesdata), [navigation.layout])[0];
+      res.status(200).send({
+        result: 'success',
+        status: 200,
+        data: {
+          settings: NAVIGATION,
+        },
+      });
+    })
+    .catch(next);
 };
 
 module.exports = function (resources) {
+  periodic = resources;
   appSettings = resources.settings;
   appenvironment = appSettings.application.environment;
   CoreController = resources.core.controller;
