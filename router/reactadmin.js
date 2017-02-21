5f122f828afb2aef2da818f2a6b4684c7d9298ae
Yaw Joseph Etse <yaw.etse@gmail.com>
2017-02-21 12:01:47 -0500

    updated router work around

--- router/reactadmin.js
+++ router/reactadmin.js
@@ -1,11 +1,13 @@
-
+'use strict';
 
 module.exports = function(resources) {
 	// console.log('setting up react admin router');
   const reactadminController = resources.app.controller.extension.reactadmin.controller.reactadmin;
   const ReactAdminRouter = resources.express.Router();
   ReactAdminRouter.get('/', reactadminController.index);
-  ReactAdminRouter.get('/*', reactadminController.index);
+  if (!resources.app.locals.extension.reactadmin.settings.skip_catch_all_route) {
+    ReactAdminRouter.get('/*', reactadminController.index);
+  }
 
   return ReactAdminRouter;
 };
