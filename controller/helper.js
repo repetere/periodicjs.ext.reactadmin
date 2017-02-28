f19698b34f59c0093a356dd6050c74c25c691cc5
Yaw Joseph Etse <yaw.etse@gmail.com>
2017-02-27 23:18:16 -0500

    updated helpers

--- controller/helper.js
+++ controller/helper.js
@@ -20,7 +20,7 @@ const fixCodeMirrorSubmit = (req, res, next) => {
     }
     delete req.body._id;
     delete req.body.__v;
-    delete req.body.format;
+    // delete req.body.format;
   }
   Object.keys(req.body).forEach(function (key) {
     if (req.body[key] === '!!--EMPTY--single--EMTPY--!!') {
@@ -34,29 +34,18 @@ const fixCodeMirrorSubmit = (req, res, next) => {
 };
 
 const fixFlattenedSubmit = function (req, res, next) {
-  req.controllerData = req.controllerData || {};
-  let authorizationHeader = req.controllerData.authorization_header;
-  req.controllerData = req.controllerData.customer;
-  req.controllerData.authorization_header = authorizationHeader;
-  delete req.controllerData.customer;
-  var body_to_json = str2json.convert(req.body);
-  req.controllerData.encryptFields = true;
-  // console.log('req.body',req.body);
-  // console.log('body_to_json',body_to_json);
-  req.body = req.controllerData;
-  req.body.docid = body_to_json.docid;
-  delete body_to_json.identification.social_security_number;
-  var ssn = req.body.identification.social_security_number;
-  req.body.identification = merge(req.controllerData.identification, body_to_json.identification);
-  req.body.identification.social_security_number = ssn;
-  req.body.latest_contact = merge(req.controllerData.latest_contact, body_to_json.latest_contact);
-  req.body.user_account = merge(req.controllerData.user_account, body_to_json.user_account);
-  if (req.body.latest_contact.phone_number_primary) {
-    req.body.latest_contact.phone_number_primary = req.body.latest_contact.phone_number_primary.replace(/\D/gi, '');
+  if (req.query.unflatten && req.body) {
+    req.body = str2json.convert(req.body);
+    if (!req.body.docid && req.body._id) {
+      req.body.docid = req.body._id;
+    }
+    if (req.body.latest_contact.phone_number_primary) {
+      req.body.latest_contact.phone_number_primary = req.body.latest_contact.phone_number_primary.replace(/\D/gi, '');
+    }
+    delete req.body._id;
+    delete req.body._csrf;
+    delete req.body.__v;
   }
-  // delete req.body._id;
-  // delete req.body._csrf;
-  // delete req.body.__v;
   next();
 };
 
--- controller/index.js
+++ controller/index.js
@@ -4,8 +4,10 @@ const path = require('path');
 
 module.exports = function(resources) {
     const reactadmin = require(path.resolve(__dirname, './reactadmin'))(resources);
+    const helper = require(path.resolve(__dirname, './helper'))(resources);
 
     return {
         reactadmin,
+        helper,
     };
 };
