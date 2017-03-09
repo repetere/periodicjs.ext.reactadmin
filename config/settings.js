dc35ca182a8f2cf9b5f58192856a7f601c853619
Yaw Joseph Etse <yaw.etse@gmail.com>
2017-03-09 16:58:22 -0500

    Fixing default settings

--- config/settings.js
+++ config/settings.js
@@ -1,84 +1,115 @@
+'use strict';
+
+function getReactAdminConfig(options, overwrites){
+  return Object.assign({
+    'custom_css_stylesheet':false,
+    'basename': options.basename,
+    'skip_catch_all_route':false,
+    'adminPath':'/r-admin',
+    'name':'Admin Panel',
+    'title':'Admin Panel',
+    'includeCoreData': {
+      'manifest': true,
+      'navigation': false,
+    },
+    'navigationLayout':{
+      'wrapper':{
+        'style':{},
+      },
+      'container':{
+        'style':{},
+      },
+    },
+    'routerHistory':'browserHistory',
+    'application':{
+      'environment':options.environment,
+      'use_offline_cache':options.cache || false,
+    },
+    'ui':{
+      'initialization':{
+        'show_header':false,
+        'show_footer':false,
+        'show_sidebar_overlay':true,
+        'refresh_manifests':false,
+        'refresh_navigation':false,
+        'refresh_components':false,
+      },
+      'notifications':{
+        'error_timeout':10000,
+        'timed_timeout':10000,
+      },
+      'fixedSidebar':true,
+      'sidebarBG':'#ffffff',
+      'header':{
+        'isBold':false,
+        'color':'isBlack',
+        'buttonColor':'isWhite',
+        'useGlobalSearch':false,
+        'useHeaderLogout':false,
+        'customButton':false,
+        'navLabelStyle':{},
+        'containerStyle':{},
+        'userNameStyle':{},
+      },
+      'footer':{
+        'navStyle':{
+        },
+      },
+      'sidebar':{
+        'containerStyle':{},
+        'use_floating_nav':false,
+      },
+    },
+    'login':{
+      'url':options.loginurl,
+      'options':{
+        'method':'POST',
+        'headers':options.loginheaders,
+      },
+    },
+    'userprofile':{
+      'url':options.profileurl,
+      'options':{
+        'method':'POST',
+        'headers':options.profileheaders,
+      },
+    },   
+  }, overwrites);
+}
+
 module.exports = {
-  'basename': 'http://localhost:8786',
-  'adminPath':'/r-admin',
-  'name':'Admin Panel',
-  'title':'Welcome',
-  'includeCoreData': {
-    'manifest': true,
-    'navigation': true,
-  },
-  'navigationLayout':{
-    'wrapper':{
-      'style':{},
+  test: getReactAdminConfig({
+    environment: 'test',
+    basename:'http://localhost:8786',
+    loginurl:'http://localhost:8786/api/jwt/token',
+    loginheaders:{
+      'Accept':'application/json',
+      'clientid':'CLIENT**ID**NEEDED',
+      'entitytype':'account',
     },
-    'container':{
-      'style':{},
+    profileurl:'http://localhost:8786/api/jwt/profile',
+    profileheaders:{
+      'Accept':'application/json',
+      'Content-Type':'application/json',
+      'clientid':'CLIENT**ID**NEEDED',
+      'entitytype':'account',
     },
-  },
-  'ui':{
-    'initialization':{
-      'show_header':false,
-      'show_footer':false,
-      'show_sidebar_overlay':true,
-      'refresh_manifests':false,
-      'refresh_navigation':false,
-      'refresh_components':false,
+  }, {}),
+  development:getReactAdminConfig({
+    environment: 'development',
+    basename:'http://localhost:8786',
+    loginurl:'http://localhost:8786/api/jwt/token',
+    loginheaders:{
+      'Accept':'application/json',
+      'clientid':'CLIENT**ID**NEEDED',
+      'entitytype':'account',
     },
-    'notifications':{
-      'error_timeout':10000,
-      'timed_timeout':10000,
+    profileurl:'http://localhost:8786/api/jwt/profile',
+    profileheaders:{
+      'Accept':'application/json',
+      'Content-Type':'application/json',
+      'clientid':'CLIENT**ID**NEEDED',
+      'entitytype':'account',
     },
-    'fixedSidebar':true,
-    'sidebarBG':'#ffffff',
-    'header':{
-      'isBold':true,
-      'color':'isBlack',
-      'buttonColor':'isWhite', 
-      'useGlobalSearch':false,
-      'useHeaderLogout':false,
-      'customButton':false,
-      'navLabelStyle':{},
-      'containerStyle':{},
-      'userNameStyle':{},
-    },
-    'footer':{
-      'navStyle':{},
-    },
-    'sidebar':{
-      'containerStyle':{},
-      'use_floating_nav':false,
-    },
-  },
-  'routerHistory':'browserHistory',
-  'allHistoryOptions':'browserHistory|hashHistory|createMemoryHistory',
-  'application':{
-    'environment':'development',
-    'use_offline_cache':false,
-  },
-  'login':{
-    'url':'http://localhost:8786/api/jwt/token',
-    'devurl':'http://localhost:8786/api/jwt/token',
-    'options':{
-      'method':'POST',
-      'headers':{
-        'Accept':'application/json',
-        'clientid':'fbff80bd23de5b1699cb595167370a1a',
-        'entitytype':'account',
-      },
-    },
-  },
-  'userprofile':{
-    'url':'http://localhost:8786/api/jwt/profile',
-    'devurl':'http://localhost:8786/api/jwt/profile',
-    'options':{
-      'method':'POST',
-      'headers':{
-        'Accept':'application/json',
-        'Content-Type':'application/json',
-        'clientid':'fbff80bd23de5b1699cb595167370a1a',
-        'clientid_default':'clientIDNEEDED',
-        'entitytype':'account',
-      },
-    },
-  },   
+  }, {}), 
 };
