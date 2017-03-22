ee1aa87c9b2eba3980fedbb5d2bb5a61ef41f7a8
Yaw Joseph Etse <yaw.etse@gmail.com>
2017-03-22 16:59:17 -0400

    adding hot reloading logs

--- utility/reloader.js
+++ utility/reloader.js
@@ -6,33 +6,34 @@ const CACHE = require.cache;
 const WATCHED = new Map();
 
 var debounce = function (fn) {
-	let timeout;
-	return function () {
-		if (!timeout) timeout = setTimeout(fn, 1500);
-		else {
-			clearTimeout(timeout);
-			timeout = setTimeout(fn, 1500);
-		}
-	};
+  let timeout;
+  return function () {
+    if (!timeout) timeout = setTimeout(fn, 1500);
+    else {
+      clearTimeout(timeout);
+      timeout = setTimeout(fn, 1500);
+    }
+  };
 };
 
 var flushCached = function (filepath, fn, isModule) {
-	return debounce(() => {
-		if (isModule) delete CACHE[filepath];
-		if (typeof fn === 'function') fn();
-	});
+  return debounce(() => {
+    if (isModule) delete CACHE[ filepath ];
+    if (typeof fn === 'function') fn();
+    console.log('::HOT RELOAD::', { filepath, });
+  });
 };
 
 var watcher = function (filepath, onChange) {
-	let isModule = (path.extname(filepath) === '.js');
-	if (!WATCHED.has(filepath)) {
-		let listener;
-		if (typeof onChange !== 'function') listener = flushCached(filepath, null, isModule);
-		else listener = flushCached(filepath, onChange, isModule);
-		fs.watch(filepath, 'utf8', listener);
-		WATCHED.set(filepath, true);
-	}
-	return (isModule) ? Promisie.resolve(require(filepath)) : fs.readJsonAsync(filepath);
+  let isModule = (path.extname(filepath) === '.js');
+  if (!WATCHED.has(filepath)) {
+    let listener;
+    if (typeof onChange !== 'function') listener = flushCached(filepath, null, isModule);
+    else listener = flushCached(filepath, onChange, isModule);
+    fs.watch(filepath, 'utf8', listener);
+    WATCHED.set(filepath, true);
+  }
+  return (isModule) ? Promisie.resolve(require(filepath)) : fs.readJsonAsync(filepath);
 };
 
 module.exports = watcher;
