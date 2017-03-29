79973b255751f602dd161faf2a29a4be9be5ce13
Yaw Joseph Etse <yaw.etse@gmail.com>
2017-03-29 17:32:35 -0400

    updating console catch for logs

--- utility/ssr_manifest_es6.js
+++ utility/ssr_manifest_es6.js
@@ -6,14 +6,15 @@
 //babel adminclient/src -d adminclient/_src --ignore adminclient/src/components/RACodeMirror/
 require('isomorphic-fetch');
 import React from 'react';
-import { renderToString } from 'react-dom/server'
+import { renderToString, } from 'react-dom/server';
 import SSR from '../adminclient/_src/containers/SSR';
 import utility from '../adminclient/_src/util';
+console.debug = console.log;
 
 module.exports = function (options) {
   return new Promise((resolve, reject) => {
     try {
-      let { layoutPath, manifest, req_url, basename } = options;
+      let { layoutPath, manifest, req_url, basename, } = options;
       if (manifest && manifest.layout) {
         if (Object.keys(manifest.resources).length) {
           let resources = utility._handleDynamicParams(layoutPath, manifest.resources, req_url);
@@ -22,18 +23,18 @@ module.exports = function (options) {
           utility.fetchPaths(basename, resources, {})
             .then(_resources => {
               // console.log({ _resources });
-              let dyanmicManifest = Object.assign({}, manifest, { resources: _resources });
+              let dyanmicManifest = Object.assign({}, manifest, { resources: _resources, });
               const body = renderToString(<SSR {...dyanmicManifest} />);
-              resolve({ body, pagedata: manifest.pageData });
+              resolve({ body, pagedata: manifest.pageData, });
             })
             .catch((e) => {
               console.error(e);
               const body = renderToString(<SSR {...manifest} />);
-              resolve({ body, pagedata: manifest.pageData });
+              resolve({ body, pagedata: manifest.pageData, });
             });
         } else {
           const body = renderToString(<SSR {...manifest} />);
-          resolve({body, pagedata:manifest.pageData});   
+          resolve({ body, pagedata:manifest.pageData, });   
         }
       } else {
         resolve({});
