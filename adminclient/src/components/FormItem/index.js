398a0d43a6ac6499368109211111242c992d5e9f
Iris Yoojung Dan <yjdan92@gmail.com>
2017-04-06 17:48:48 -0400

    added hasError and hasValue classes to FormItems

--- adminclient/src/components/FormItem/index.js
+++ adminclient/src/components/FormItem/index.js
@@ -3,16 +3,18 @@ import { Column, FormHorizontal, } from 're-bulma';
 
 class FormItem extends Component{
   render() {
+    let className = (this.props.hasValue)? '__form_element_has_value':'';
+    className = (this.props.hasError)? `${(className)? `${className} `:''}__form_element_has_error`:`${className}`;
     if (this.props.innerFormItem) {
       return (this.props.horizontalform)
-        ? (<FormHorizontal style={Object.assign({ width:'100%',  }, this.props.formItemStyle)}>{this.props.children}</FormHorizontal>)
-        : (<span style={Object.assign({ width:'100%',  }, this.props.formItemStyle)}>{this.props.children}</span>);
+        ? (<FormHorizontal className={className} style={Object.assign({ width:'100%',  }, this.props.formItemStyle)}>{this.props.children}</FormHorizontal>)
+        : (<span className={className} style={Object.assign({ width:'100%',  }, this.props.formItemStyle)}>{this.props.children}</span>);
     } else {
       return <Column {...this.props}>
         {
           (this.props.horizontalform)
-            ? (<FormHorizontal style={Object.assign({ width:'100%',  }, this.props.formItemStyle)}>{this.props.children}</FormHorizontal>)
-            : (<span style={Object.assign({ width:'100%',  }, this.props.formItemStyle)}>{this.props.children}</span>)
+            ? (<FormHorizontal className={className} style={Object.assign({ width:'100%',  }, this.props.formItemStyle)}>{this.props.children}</FormHorizontal>)
+            : (<span className={className} style={Object.assign({ width:'100%',  }, this.props.formItemStyle)}>{this.props.children}</span>)
         }
       </Column>;  
     }
