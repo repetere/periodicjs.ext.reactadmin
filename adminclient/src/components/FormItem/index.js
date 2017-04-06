b2bc0f82f439d1391d134671b448fbdfa1a5d3b0
Iris Dan <dany@bc.edu>
2017-04-06 17:59:12 -0400

    got rid of unnecessary interpolation

--- adminclient/src/components/FormItem/index.js
+++ adminclient/src/components/FormItem/index.js
@@ -4,7 +4,7 @@ import { Column, FormHorizontal, } from 're-bulma';
 class FormItem extends Component{
   render() {
     let className = (this.props.hasValue)? '__form_element_has_value':'';
-    className = (this.props.hasError)? `${(className)? `${className} `:''}__form_element_has_error`:`${className}`;
+    className = (this.props.hasError)? `${(className)? `${className} `:''}__form_element_has_error`:className;
     if (this.props.innerFormItem) {
       return (this.props.horizontalform)
         ? (<FormHorizontal className={className} style={Object.assign({ width:'100%',  }, this.props.formItemStyle)}>{this.props.children}</FormHorizontal>)
