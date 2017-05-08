2c6e1651d4616da9b93a45e4e062d960321b8383
Iris Yoojung Dan <yjdan92@gmail.com>
2017-05-08 13:57:34 -0400

    got rid of chart

--- views/test/dynamicform.manifest.js
+++ views/test/dynamicform.manifest.js
@@ -1,73 +1,5 @@
 'use strict';
 
-function getTestChart(defaultDisplay) {
-  const data = [
-    { name: 'A', uv: 4000, pv: 2400, amt: 2400, },
-    { name: 'B', uv: 3000, pv: 1398, amt: 2210, },
-    { name: 'C', uv: 2000, pv: 9800, amt: 2290, },
-    { name: 'D', uv: 2780, pv: 3908, amt: 2000, },
-    { name: 'E', uv: 1890, pv: 4800, amt: 2181, },
-    { name: 'F', uv: 2390, pv: 3800, amt: 2500, },
-    { name: 'G', uv: 3490, pv: 4300, amt: 2100, },
-  ];
-
-  return {
-    component: 'DynamicChart',
-    props: {
-      passProps: {
-        width: 600,
-        height: 300,
-      },
-      chartComponent: 'LineChart',
-      chartProps: {
-        width: 600,
-        height: 300,
-        data: [
-          { name: 'A', uv: 4000, pv: 2400, amt: 2400, },
-          { name: 'B', uv: 3000, pv: 1398, amt: 2210, },
-          { name: 'C', uv: 2000, pv: 9800, amt: 2290, },
-          { name: 'D', uv: 2780, pv: 3908, amt: 2000, },
-          { name: 'E', uv: 1890, pv: 4800, amt: 2181, },
-          { name: 'F', uv: 2390, pv: 3800, amt: 2500, },
-          { name: 'G', uv: 3490, pv: 4300, amt: 2100, },
-        ],
-      },
-    },
-    children: [{
-      component: 'recharts.XAxis',
-      props: {
-        dataKey: 'name',
-      },
-    }, {
-      component: 'recharts.YAxis',
-    }, {
-      component: 'recharts.CartesianGrid',
-      props: {
-        strokeDasharray: '3 3',
-      },
-    }, {
-      component: 'recharts.Tooltip',
-    }, {
-      component: 'recharts.Legend',
-    }, {
-      component: 'recharts.Line',
-      props: {
-        type: 'monotone',
-        dataKey: 'pv',
-        stroke: '#8884d8',
-        activeDot: { r: 8, },
-      },
-    }, {
-      component: 'recharts.Line',
-      props: {
-        type: 'monotone',
-        dataKey: 'uv',
-        stroke: '#82ca9d',
-      },
-    }, ],
-  };
-}
-
 module.exports = {
   'containers': {
     '/r-admin/content/testdynamicform': {
@@ -87,7 +19,7 @@ module.exports = {
             props: {},
             children: [{
                 component: 'Title',
-                children: 'Chart Test',
+                children: 'Dynamic Response Form Test',
                 thisprops: {
                   reqUser: ['user', ],
                 },
@@ -109,13 +41,6 @@ module.exports = {
                     operation: 'lte',
                     right: 1080,
                   },
-                ],
-              },
-              {
-                component: 'div',
-                props: {},
-                children: [
-                  getTestChart(),
                 ],
               },
               {
