3c953e2cd86aed68b422fb6aae4e10b4d0e44687
Iris Yoojung Dan <yjdan92@gmail.com>
2017-09-08 16:42:39 -0400

    ability to pass in params for responsive button createmodal

--- adminclient/_src/components/ResponsiveButton/index.js
+++ adminclient/_src/components/ResponsiveButton/index.js
@@ -135,6 +135,9 @@ var ResponsiveButton = function (_Component) {
         onclickFunction = clickprop;
       }
       // onclickFunction = onclickFunction.bind(this);
+      if (typeof clickprop === 'string' && clickprop === 'func:this.props.createModal') {
+        onclickProp.pathname = onclickProp.params ? this.getButtonLink(onclickProp.pathname, onclickProp.params, linkSelectionProp) : onclickProp.pathname;
+      }
       if (this.props.confirmModal) {
         return this.props.createModal((0, _assign2.default)({
           title: 'Please Confirm',
